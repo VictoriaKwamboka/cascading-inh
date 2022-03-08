68306bfed7f73190f68c3dc10f236e14c6f9180f
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-03-08 17:51:37 +0300

    Add buttons to change to light and dark mode

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('button#light-mode').click(function(){
+        $('body').removeClass();
+        $('body').addClass('light-bgd')
+    });
+    $('button#dark-mode').click(function(){
+        $('body').removeClass();
+        $('body').addClass('dark-bgd')
+    });
+});
